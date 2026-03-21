c843dec57e78e74677c2ee62dfeafc266ded5ed2
akbarazy <muh.akbar.alfarizy@gmail.com>
2026-03-21 09:21:30 +0700

    Add C++ and Javascript solve code

---
+++ invert-binary-tree/C++/main.cpp
@@ -0,0 +1,36 @@
+/*
+Given the root of a binary tree, invert the tree, and return its root.
+
+Example 1
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+
+Example 2
+Input: root = [2,1,3]
+Output: [2,3,1]
+
+Example 3
+Input: root = []
+Output: []
+*/
+
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+};
+
+TreeNode *invertTree(TreeNode *root) {
+    if(root == nullptr) return nullptr;
+
+    TreeNode *left = invertTree(root->left);
+    TreeNode *right = invertTree(root->right);
+
+    root->left = right;
+    root->right = left;
+
+    return root;
+}
